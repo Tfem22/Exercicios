83d41e2f5764dd5bc354977f2f11f345e53aa14e
Thiago Maciel <fonterrada15@gmail.com>
2021-02-19 23:45:24 -0300

    quantos A em uma frase

---
+++ ex.026.py
@@ -0,0 +1,4 @@
+#conta qtd de A em uma frase
+frase=input('digite uma frase: ').strip()
+print('essa frase contem {} letras A'.format(frase.lower().count('a')))
+print('o primeiro a aparece no caractere {}, e o ultimo {}'.format(frase.find('a'),frase.rfind('a')))
