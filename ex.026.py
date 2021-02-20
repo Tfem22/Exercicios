b097bdf41d5e585da4c197af19c917c8191b3277
Thiago Maciel <fonterrada15@gmail.com>
2021-02-20 00:04:49 -0300

    atualizando

--- ex.026.py
+++ ex.026.py
@@ -1,4 +1,4 @@
 #conta qtd de A em uma frase
-frase=input('digite uma frase: ').strip()
+frase=input('digite uma frase: ').strip().lower()
 print('essa frase contem {} letras A'.format(frase.lower().count('a')))
 print('o primeiro a aparece no caractere {}, e o ultimo {}'.format(frase.find('a'),frase.rfind('a')))
