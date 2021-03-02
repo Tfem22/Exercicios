16b421b37028e6bdd19f25905a78204a3c5e7380
Thiago Maciel <fonterrada15@gmail.com>
2021-03-02 20:29:17 -0300

    triangulo

--- ex.035.py
+++ ex.035.py
@@ -2,9 +2,7 @@ lado1=int(input('lado a: '))
 lado2=int(input('lado b: '))
 lado3=int(input('lado c: '))
 
-if lado1==lado2 or lado2==lado3 or lado1==lado3:
-    print('é possivel formar um triangulo')
-elif lado1==lado2 and lado1==lado3:
-    print(' é possivel formar um triangulo')
+if (lado1+lado2)>lado3 and (lado2+lado3)>lado1 and (lado1+lado3)>lado2:
+    print('dado as retas a,b e c, é possivel formar um trigangulo')
 else:
-    print('não é possivel formar um triangulo')
+    print('dado as retas a,b e c, não é possivel formar um triangulo')
